d8c30f6f459bdee2aeb1c4fe0ec1bd0837df3bed
k-himeno <47313624+k-himeno@users.noreply.github.com>
2023-10-12 16:42:04 +0900

    とりあえずのクラス化

--- souces/make_pass.py
+++ souces/make_pass.py
@@ -21,133 +21,126 @@ except ImportError:
     GUI_AVAILABLE = False
 
 
-def make_password(
-    master1, master2, seed="1234", plen=20, char="ans", rm_cha="", start=1, upper=False, lower=False, **kwargs
-):
-    """パスワードを生成する関数
+class PasswordGenerator:
+    def __init__(self, csv_path):
+        self.csv_path = csv_path
+        self.master1, self.master2 = self.input_master()
 
-    Args:
-        master1 (str): マスターパスワード1
-        master2 (str): マスターパスワード2
-        seed (str, optional): サービスごとのパスワード. Defaults to "1234".
-        plen (int, optional): パスワードの長さ． Defaults to 20.
-        char (str, optional): a[アルファベット], n[数字], s[記号]の組み合わせ. Defaults to "ans".
-        rm_cha (bool, optional): NG記号. Defaults to "".
-        start (int, optional): パスワードの開始位置. Defaults to 1.
-        upper (bool, optional): 大文字縛りがあるか. Defaults to False.
-        lower (bool, optional): 小文字縛りがあるか. Defaults to False.
-    """
+    def make_password(
+        self, master1, master2, seed="1234", plen=20, char="ans", rm_cha="", start=1, upper=False, lower=False, **kwargs
+    ):
+        """パスワードを生成する関数
 
-    # 指定されたハッシュ関数を適用する。
-    # https://docs.python.jp/3/library/hashlib.html
+        Args:
+            master1 (str): マスターパスワード1
+            master2 (str): マスターパスワード2
+            seed (str, optional): サービスごとのパスワード. Defaults to "1234".
+            plen (int, optional): パスワードの長さ． Defaults to 20.
+            char (str, optional): a[アルファベット], n[数字], s[記号]の組み合わせ. Defaults to "ans".
+            rm_cha (bool, optional): NG記号. Defaults to "".
+            start (int, optional): パスワードの開始位置. Defaults to 1.
+            upper (bool, optional): 大文字縛りがあるか. Defaults to False.
+            lower (bool, optional): 小文字縛りがあるか. Defaults to False.
+        """
 
-    seed_tmp = master1 + seed + master2
-    h = hashlib.sha512(seed_tmp.encode("utf-8"))
+        # 指定されたハッシュ関数を適用する。
+        # https://docs.python.jp/3/library/hashlib.html
 
-    # 指定された文字の種類にしたがって、パスワードの文字列を作る。
+        seed_tmp = master1 + seed + master2
+        h = hashlib.sha512(seed_tmp.encode("utf-8"))
 
-    base = base64.b85encode(h.digest()).decode("utf-8")  # b85
-    # !#$%&()*+-;<=>?@^_`{|}~ と文字数字
+        # 指定された文字の種類にしたがって、パスワードの文字列を作る。
 
-    if char == ("ans"):  # base85
-        p = base
-    elif char == ("an"):  # an: アルファベットと数字
-        p = re.sub(r"[^a-zA-Z0-9]", "", base)
-    elif char == ("a"):  # Aa: アルファベット
-        p = re.sub(r"[^a-zA-Z]", "", base)
-    elif char == ("n"):  # n: 数字
-        # base64
-        base = base64.b64encode(h.digest()).decode("utf-8")
-        p = re.sub(r"[^0-9]", "", base)
-    else:
-        raise ValueError(f"char {char} は定義されていません。")
+        base = base64.b85encode(h.digest()).decode("utf-8")  # b85
+        # !#$%&()*+-;<=>?@^_`{|}~ と文字数字
 
-    # rm_chaをpから削除
-    for i in rm_cha:
-        p = p.replace(i, "")
+        if char == ("ans"):  # base85
+            p = base
+        elif char == ("an"):  # an: アルファベットと数字
+            p = re.sub(r"[^a-zA-Z0-9]", "", base)
+        elif char == ("a"):  # Aa: アルファベット
+            p = re.sub(r"[^a-zA-Z]", "", base)
+        elif char == ("n"):  # n: 数字
+            # base64
+            base = base64.b64encode(h.digest()).decode("utf-8")
+            p = re.sub(r"[^0-9]", "", base)
+        else:
+            raise ValueError(f"char {char} は定義されていません。")
 
-    if upper:
-        p = p.upper()
-    if lower:
-        p = p.lower()
+        # rm_chaをpから削除
+        for i in rm_cha:
+            p = p.replace(i, "")
 
-    # 指定された長さのパスワードを表示する。
-    print(p[start : start + plen])
+        if upper:
+            p = p.upper()
+        if lower:
+            p = p.lower()
 
-    return {"password": p[start : start + plen], "all_chr": base}
+        # 指定された長さのパスワードを表示する。
+        print(p[start : start + plen])
 
+        return {"password": p[start : start + plen], "all_chr": base}
 
-def input_master():
-    """マスターパスワードの入力を受け付け，チェック"""
+    def input_master(self):
+        """マスターパスワードの入力を受け付け，チェック"""
 
-    flag = True
-    while flag:
-        print("マスター1を入力: ")
-        master1 = input()
-        print("マスター2を入力: ")
-        master2 = input()
-        _ = make_password(master1, master2, seed="1234", plen=20, char="ans", rm_cha="", start=1)
-        print("マスターパスワードは正しいですか？(y/n)")
-        flag = input() == "n"
-    return {"master1": master1, "master2": master2}
+        flag = True
+        while flag:
+            print("マスター1を入力: ")
+            master1 = input()
+            print("マスター2を入力: ")
+            master2 = input()
+            _ = self.make_password(master1, master2, seed="1234", plen=20, char="ans", rm_cha="", start=1)
+            print("マスターパスワードは正しいですか？(y/n)")
+            flag = input() == "n"
+        return master1, master2
 
+    def get_input_site_info(self):
+        """サイトの情報を入力する"""
 
-def get_input_site_info_cui():
-    """サイトの情報を入力する"""
+        site_name = input("Enter site name: ")
+        id = input("Enter id: ")
+        seed = input("Enter seed: ")
+        plen = input("Enter length (default is 20): ")
+        plen = int(plen) if plen else 20
+        char = input("Enter char (ans/an/a/n): ")
 
-    site_name = input("Enter site name: ")
-    id = input("Enter id: ")
-    seed = input("Enter seed: ")
-    plen = input("Enter length (default is 20): ")
-    plen = int(plen) if plen else 20
-    char = input("Enter char (ans/an/a/n): ")
+        # 更新した日も記録
+        update_date = time.strftime("%Y/%m/%d", time.localtime())
+        return {"site_name": site_name, "id": id, "seed": seed, "plen": plen, "char": char, "update_date": update_date}
 
-    # 更新した日も記録
-    update_date = time.strftime("%Y/%m/%d", time.localtime())
-    return {"site_name": site_name, "id": id, "seed": seed, "plen": plen, "char": char, "update_date": update_date}
-
-
-def get_input_site_info():
-    """サイトの情報を入力する"""
-    if GUI_AVAILABLE:
-        print("GUI is not available yet.")
-        site_info_dict = get_input_site_info_cui()
-    else:
-        site_info_dict = get_input_site_info_cui()
-    return site_info_dict
-
-
-def append_dict_to_csv(filename, data_dict):
-    try:
-        df = pd.read_csv(filename)
-    except FileNotFoundError:
-        df = pd.DataFrame()
-
-    # 現在の列と新しいデータのキーを取得
-    existing_columns = set(df.columns)
-    new_data_keys = set(data_dict.keys())
-
-    # 新しいデータにはあるがCSVにはない列を特定
-    for col in new_data_keys - existing_columns:
-        df[col] = ""
-
-    # 新しい行をDataFrameに追加
-    df = pd.concat([df, pd.DataFrame([data_dict])], ignore_index=True)
-    # CSVファイルに保存
-    while True:
+    def append_dict_to_csv(self, data_dict):
         try:
-            df.to_csv(filename, index=False)
-            break
-        except PermissionError:
-            input("ファイルがロックされています。ファイルを閉じて、Enterキーを押してください。")
+            df = pd.read_csv(self.csv_path)
+        except FileNotFoundError:
+            df = pd.DataFrame()
+
+        # 現在の列と新しいデータのキーを取得
+        existing_columns = set(df.columns)
+        new_data_keys = set(data_dict.keys())
+
+        # 新しいデータにはあるがCSVにはない列を特定
+        for col in new_data_keys - existing_columns:
+            df[col] = ""
+
+        # 新しい行をDataFrameに追加
+        df = pd.concat([df, pd.DataFrame([data_dict])], ignore_index=True)
+        # CSVファイルに保存
+        while True:
+            try:
+                df.to_csv(self.csv_path, index=False)
+                break
+            except PermissionError:
+                input("ファイルがロックされています。ファイルを閉じて、Enterキーを押してください。")
+
+    def run(self):
+        site_info = self.get_input_site_info()
+        password_data = self.make_password(self.master1, self.master2, **site_info)
+        print(password_data["password"])
+        self.append_dict_to_csv(site_info)
 
 
-masters = input_master()
-site_info = get_input_site_info()
-print(site_info)
-
-
-make_password(**masters, **site_info)
-
-path = "hash_key.csv"
-append_dict_to_csv(path, site_info)
+if __name__ == "__main__":
+    path = "hash_key.csv"
+    generator = PasswordGenerator(path)
+    generator.run()
